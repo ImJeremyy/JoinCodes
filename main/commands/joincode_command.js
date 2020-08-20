7813e0c1a29541d2d7a87cba3429365f029b4783
ImJeremyy <madivoso@gmail.com>
2020-08-20 09:50:37 -0400

    Added redis code

--- main/commands/joincode_command.js
+++ main/commands/joincode_command.js
@@ -1,6 +1,7 @@
 const CommandBase = require("./command_base");
 const util = require("../utility/util");
 const json = require("../utility/json");
+const client = require("redis").createClient(process.env.REDIS_URL);
 
 class JoinCodeCommand extends CommandBase {
 	constructor() {
@@ -20,13 +21,16 @@ class JoinCodeCommand extends CommandBase {
 			channel.send(user.toString() + " Invalid command usage. .joincode <@user>");
 			return;
 		}
-		const read = json.read(json.usersPath + "\\" + id + ".json");
-		if (read != null) {
-			const code = read.joinCode;
+		client.get(id, (err, reply) => {
+			if (err != null) {
+				console.log("Attempted code fetch. id: " + id + " reply.toString(): " + reply.toString());
+				channel.send(user.toString() + " Join code was not registered. Register with **.registercode <join_code>**");
+				return;
+			}
+			const code = reply.toString();
 			channel.send(user.toString() + "\n```/join " + code + "```");
-		} else {
-			channel.send(user.toString() + " This user has not registered their join code");
-		}
+			console.log("Successful fetch of id:" + id + " code: " + code);
+		});
 	}
 }
 
--- main/commands/registercode_command.js
+++ main/commands/registercode_command.js
@@ -1,5 +1,5 @@
 const CommandBase = require("./command_base");
-const json = require("../utility/json");
+const client = require("redis").createClient(process.env.REDIS_URL);
 
 class RegisterCodeCommand extends CommandBase {
 	constructor() {
@@ -11,8 +11,8 @@ class RegisterCodeCommand extends CommandBase {
 		const channel = message.channel;
 		if (args.length == 1) {
 			const joinCode = args[0];
-			json.write(json.usersPath + "\\" + user.id, { joinCode: joinCode });
-			channel.send(user.toString() + " Registered your join code: " + joinCode + ". Now do .joincode " + user.toString());
+			client.set(user.id, joinCode);
+			channel.send(user.toString() + " Registered your join code: " + joinCode + ". Now do **.joincode** to view your join code.");
 			console.log("Registered code of id:" + user.id + " tag: " + user.tag + " with joinCode: " + joinCode);
 		} else {
 			channel.send(user.toString() + " Invalid command usage. .registercode <your_join_code>");
