5580bbaabb1b60862bfc8f9608afd8cc6eb482d5
ImJeremyy <madivoso@gmail.com>
2020-08-19 21:37:29 -0400

    Heroku test changes

--- main/app.js
+++ main/app.js
@@ -4,8 +4,8 @@ const json = require("./utility/json");
 const bot = new Client();
 
 bot.on("ready", () => {
-	console.log("Lemons");
+	console.log("Bot activated");
 });
 
 console.log("Loading..");
-bot.login(json.token);
+bot.login(process.env.token);
