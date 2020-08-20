6f9edc72103a6d0d1a09c0bd491a7dc0f44be3f0
ImJeremyy <madivoso@gmail.com>
2020-08-20 09:30:41 -0400

    Cleaned join code command

--- main/commands/joincode_command.js
+++ main/commands/joincode_command.js
@@ -10,23 +10,21 @@ class JoinCodeCommand extends CommandBase {
 	run(message, args) {
 		const user = message.author;
 		const channel = message.channel;
+		var id;
 		if (args.length == 0) {
-			const code = json.read(json.usersPath + "\\" + user.id + ".json").joinCode;
-			if (code != null) {
-				channel.send(user.toString() + " Your join code is " + code + "\n```/join " + code + "```");
-			} else {
-				channel.send(user.toString() + " This user has not registered their join code");
-			}
+			id = user.id;
 		} else if (args.length == 1) {
-			const targetId = util.getIdFromAt(args[0]);
-			const code = json.read(json.usersPath + "\\" + targetId + ".json").joinCode;
-			if (code != null) {
-				channel.send(user.toString() + " Their join code is " + code + "\n```/join " + code + "```");
-			} else {
-				channel.send(user.toString() + " This user has not registered their join code");
-			}
+			id = util.getIdFromAt(args[0]);
 		} else {
 			channel.send(user.toString() + " Invalid command usage. .joincode <@user>");
+			return;
+		}
+		const read = json.read(json.usersPath + "\\" + targetId + ".json");
+		if (read != null) {
+			const code = read.joinCode;
+			channel.send(user.toString() + "\n```/join " + code + "```");
+		} else {
+			channel.send(user.toString() + " This user has not registered their join code");
 		}
 	}
 }
