1d11587120eaf43077b16ea41369b270ab14f87f
ImJeremyy <madivoso@gmail.com>
2020-08-19 22:17:26 -0400

    Added log messages

--- main/commands/registercode_command.js
+++ main/commands/registercode_command.js
@@ -13,6 +13,7 @@ class RegisterCodeCommand extends CommandBase {
 			const joinCode = args[0];
 			json.write(json.usersPath + "\\" + user.id, { joinCode: joinCode });
 			channel.send(user.toString() + " Registered your join code: " + joinCode + ". Now do .joincode " + user.toString());
+			console.log("Registered code of id:" + user.id + " tag: " + user.tag + " with joinCode: " + joinCode);
 		} else {
 			channel.send(user.toString() + " Invalid command usage. .registercode <your_join_code>");
 		}
